9faf578c04de83d8490dc9bbcc8c2bd16328cb21
Wei Dong <weidong@andrew.cmu.edu>
2021-05-26 16:09:08 -0400

    add voxelization analyzer

---
+++ examples/hashmap/analyze_voxelization.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"]
+})
+# for Palatino and other serif fonts use:
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Palatino"],
+})
+
+data_8m = np.array([0.05,0.9782,1.6225,4.0575,
+                    0.04,1.0280,1.5994,5.4288,
+                    0.03,1.0057,1.5216,6.7097,
+                    0.02,1.1537,1.7218,9.6999,
+                    0.01,1.2900,1.7130,27.4152,
+                    0.005, 1.8106, 1.8783, 63.8137]).reshape((6, -1))
+data_500k = np.array([0.05,0.5380,0.7093,1.8193,
+                      0.04,0.5165,0.7208,1.9937,
+                      0.03,0.5184,0.7307,2.0288,
+                      0.02,0.5435,0.7392,2.8473,
+                      0.01,0.6133,0.8166,7.8951,
+                      0.005, 0.8376, 1.1206, 22.1724]).reshape((6, -1))
+x = data_8m[:, 0] * 1000
+
+colors = ['#ff000020', '#00ff0020', '#0000ff20', '#ffff0020']
+
+# Marker label first
+plt.plot(x, data_8m[:, 1], 'xb', label=r'$8\times 10^6$ inputs')
+plt.plot(x, data_500k[:, 1], 'ob', label=r'$5\times 10^5$ inputs')
+
+# Then colors label
+plt.plot(x, data_8m[:, 1], '-b', label='Ours')
+plt.plot(x, data_8m[:, 2], '-g', label='MinkowskiEngine')
+plt.plot(x, data_8m[:, 3], '-r', label='Open3D')
+
+
+plt.plot(x, data_8m[:, 1], 'x-b')
+plt.plot(x, data_8m[:, 2], 'x-g')
+plt.plot(x, data_8m[:, 3], 'x-r')
+
+plt.plot(x, data_500k[:, 1], 'o-b')
+plt.plot(x, data_500k[:, 2], 'o-g')
+plt.plot(x, data_500k[:, 3], 'o-r')
+
+plt.fill(np.append(x, x[::-1]),
+         np.append(data_8m[:, 1], data_8m[:, 2][::-1]),
+         color=colors[3])
+plt.fill(np.append(x, x[::-1]),
+         np.append(data_500k[:, 1], data_500k[:, 2][::-1]),
+         color=colors[3])
+
+plt.yscale('log')
+plt.ylabel('Time (ms)')
+plt.xlabel('Voxel size (mm)')
+plt.grid()
+plt.tight_layout()
+plt.legend()
+plt.show()
+
--- examples/hashmap/test_voxeldownsample.py
+++ examples/hashmap/test_voxeldownsample.py
@@ -8,59 +8,65 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('pcd')
-    parser.add_argument('--voxel_size', default=0.05)
+    parser.add_argument('--runs', type=int, default=100)
+    parser.add_argument('--voxel_size', type=float, default=0.05)
+    parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args()
 
     # warm up
     pcd_o3d = o3d.t.io.read_point_cloud(args.pcd)
-    for i in range(100):
+    print(pcd_o3d)
+    for i in range(args.runs):
         pcd_o3d_cuda = pcd_o3d.cuda()
         pcd_o3d_cuda.voxel_down_sample(args.voxel_size)
         torch.cuda.synchronize()
 
     pcd_legacy = pcd_o3d.to_legacy_pointcloud()
     start = time.time()
-    for i in range(100):
+    for i in range(args.runs):
         pcd_legacy.voxel_down_sample(args.voxel_size)
     end = time.time()
-    print(end - start)
-    o3d.visualization.draw_geometries(
-        [pcd_legacy.voxel_down_sample(args.voxel_size)])
+    print('open3d', end - start)
+    if args.debug:
+        o3d.visualization.draw_geometries(
+            [pcd_legacy.voxel_down_sample(args.voxel_size)])
 
     # o3d
     start = time.time()
-    for i in range(100):
+    for i in range(args.runs):
         pcd_o3d_cuda = pcd_o3d.cuda()
         pcd_o3d_cuda.voxel_down_sample(args.voxel_size)
         torch.cuda.synchronize()
     end = time.time()
-    print(end - start)
-    o3d.visualization.draw_geometries([
-        pcd_o3d.cuda().voxel_down_sample(
-            args.voxel_size).to_legacy_pointcloud()
-    ])
+    print('ours', end - start)
+    if args.debug:
+        o3d.visualization.draw_geometries([
+            pcd_o3d.cuda().voxel_down_sample(
+                args.voxel_size).to_legacy_pointcloud()
+        ])
 
-    # mink
+    # mink cuda
     pcd_mink = torch.from_numpy(pcd_o3d.point['points'].cpu().numpy())
     color_mink = torch.from_numpy(pcd_o3d.point['colors'].cpu().numpy()) / 255.0
     start = time.time()
-    for i in range(100):
+    for i in range(args.runs):
         ME.utils.sparse_quantize(pcd_mink,
                                  color_mink,
                                  quantization_size=args.voxel_size,
                                  device='cuda')
         torch.cuda.synchronize()
     end = time.time()
-    print(end - start)
+    print('mink', end - start)
 
-    pcd_down, color_down = ME.utils.sparse_quantize(
-        pcd_mink,
-        color_mink,
-        quantization_size=args.voxel_size,
-        device='cuda')
+    if args.debug:
+        pcd_down, color_down = ME.utils.sparse_quantize(
+            pcd_mink,
+            color_mink,
+            quantization_size=args.voxel_size,
+            device='cuda')
 
-    pcd_down_vis = o3d.geometry.PointCloud()
-    pcd_down_vis.points = o3d.utility.Vector3dVector(pcd_down.cpu().numpy())
-    pcd_down_vis.colors = o3d.utility.Vector3dVector(color_down.cpu().numpy())
-    o3d.visualization.draw_geometries([pcd_down_vis])
+        pcd_down_vis = o3d.geometry.PointCloud()
+        pcd_down_vis.points = o3d.utility.Vector3dVector(pcd_down.cpu().numpy())
+        pcd_down_vis.colors = o3d.utility.Vector3dVector(color_down.cpu().numpy())
+        o3d.visualization.draw_geometries([pcd_down_vis])
