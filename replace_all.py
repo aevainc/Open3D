38b656918bf7e97e0331b79d86d74f574f7598b6
Yixing Lao <yixing.lao@gmail.com>
2021-07-15 04:53:35 -0700

    replacer

--- replace_all.py
+++ replace_all.py
@@ -13,8 +13,35 @@ def get_source_files(root_dir):
     return source_files
 
 
+def replace_string_in_file(file_path, src, dst):
+    with open(file_path) as f:
+        lines = f.readlines()
+        lines = [line.replace(src, dst) for line in lines]
+
+    with open(file_path, "w") as f:
+        f.writelines(lines)
+
+
 if __name__ == '__main__':
     root_dir = pwd / "cpp"
-    source_files = get_source_files(root_dir)
-    source_files = sorted(source_files)
-    pprint(source_files)
+    file_paths = get_source_files(root_dir)
+    file_paths = sorted(file_paths)
+
+    srcs_dsts = [
+        ("core::Dtype::Undefined", "core::Undefined"),
+        ("core::Dtype::Float32", "core::Float32"),
+        ("core::Dtype::Float64", "core::Float64"),
+        ("core::Dtype::Int8", "core::Int8"),
+        ("core::Dtype::Int16", "core::Int16"),
+        ("core::Dtype::Int32", "core::Int32"),
+        ("core::Dtype::Int64", "core::Int64"),
+        ("core::Dtype::UInt8", "core::UInt8"),
+        ("core::Dtype::UInt16", "core::UInt16"),
+        ("core::Dtype::UInt32", "core::UInt32"),
+        ("core::Dtype::UInt64", "core::UInt64"),
+        ("core::Dtype::Bool", "core::Bool"),
+    ]
+
+    for src, dst in srcs_dsts:
+        for file_path in file_paths:
+            replace_string_in_file(file_path, src, dst)
