64c126c19b4639bfaf16a2b3ae4a1c6ff0e76421
Yixing Lao <yixing.lao@gmail.com>
2021-07-25 04:56:08 -0700

    format

--- cpp/open3d/t/io/NumpyIO.h
+++ cpp/open3d/t/io/NumpyIO.h
@@ -121,22 +121,29 @@ struct NpyArray {
 };
 
 char BigEndianTest();
+
 char map_type(const std::type_info& t);
+
 template <typename T>
 std::vector<char> create_npy_header(const std::vector<size_t>& shape);
+
 void parse_npy_header(FILE* fp,
                       size_t& word_size,
                       std::vector<size_t>& shape,
                       bool& fortran_order);
+
 void parse_npy_header(unsigned char* buffer,
                       size_t& word_size,
                       std::vector<size_t>& shape,
                       bool& fortran_order);
+
 void parse_zip_footer(FILE* fp,
                       uint16_t& nrecs,
                       size_t& global_header_size,
                       size_t& global_header_offset);
+
 std::map<std::string, cnpy::NpyArray> npz_load(std::string fname);
+
 NpyArray npz_load(std::string fname, std::string varname);
 
 template <typename T>
@@ -151,6 +158,7 @@ std::vector<char>& operator+=(std::vector<char>& lhs, const T rhs) {
 
 template <>
 std::vector<char>& operator+=(std::vector<char>& lhs, const std::string rhs);
+
 template <>
 std::vector<char>& operator+=(std::vector<char>& lhs, const char* rhs);
 
