a631b78e45d9384d82664e36c57412238082cf74
Yixing Lao <yixing.lao@gmail.com>
2021-07-25 05:35:31 -0700

    read tensor.npz

--- read_out_npz.py
+++ read_out_npz.py
@@ -3,8 +3,17 @@ from pathlib import Path
 import os
 
 pwd = Path(os.path.dirname(os.path.realpath(__file__)))
-npz_file = pwd / "build" / "out.npz"
 
-with np.load(npz_file) as data:
-    for key in data:
-        print(key, data[key])
+if __name__ == '__main__':
+    npz_file = pwd / "build" / "out.npz"
+    with np.load(npz_file) as data:
+        for key in data:
+            print(key, data[key])
+
+    npz_file = pwd / "build" / "tensor.npz"
+    if npz_file.exists():
+        with np.load(npz_file) as data:
+            for key in data:
+                print(key, data[key])
+    else:
+        print(f"{npz_file} not found")
