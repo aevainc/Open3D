8b0713eb846695d98901f8c6f6d8738f78e93205
Yixing Lao <yixing.lao@gmail.com>
2021-07-25 19:44:41 -0700

    simplify

--- cpp/open3d/t/io/NumpyIO.cpp
+++ cpp/open3d/t/io/NumpyIO.cpp
@@ -669,65 +669,6 @@ void parse_npy_header(unsigned char* buffer,
     word_size = atoi(str_ws.substr(0, loc2).c_str());
 }
 
-void parse_npy_header(FILE* fp,
-                      size_t& word_size,
-                      std::vector<size_t>& shape,
-                      bool& fortran_order) {
-    char buffer[256];
-    size_t res = fread(buffer, sizeof(char), 11, fp);
-    if (res != 11) throw std::runtime_error("parse_npy_header: failed fread");
-    std::string header = fgets(buffer, 256, fp);
-    assert(header[header.size() - 1] == '\n');
-
-    size_t loc1, loc2;
-
-    // fortran order
-    loc1 = header.find("fortran_order");
-    if (loc1 == std::string::npos)
-        throw std::runtime_error(
-                "parse_npy_header: failed to find header keyword: "
-                "'fortran_order'");
-    loc1 += 16;
-    fortran_order = (header.substr(loc1, 4) == "True" ? true : false);
-
-    // shape
-    loc1 = header.find("(");
-    loc2 = header.find(")");
-    if (loc1 == std::string::npos || loc2 == std::string::npos)
-        throw std::runtime_error(
-                "parse_npy_header: failed to find header keyword: '(' or ')'");
-
-    std::regex num_regex("[0-9][0-9]*");
-    std::smatch sm;
-    shape.clear();
-
-    std::string str_shape = header.substr(loc1 + 1, loc2 - loc1 - 1);
-    while (std::regex_search(str_shape, sm, num_regex)) {
-        shape.push_back(std::stoi(sm[0].str()));
-        str_shape = sm.suffix().str();
-    }
-
-    // endian, word size, data type
-    // byte order code | stands for not applicable.
-    // not sure when this applies except for byte array
-    loc1 = header.find("descr");
-    if (loc1 == std::string::npos)
-        throw std::runtime_error(
-                "parse_npy_header: failed to find header keyword: 'descr'");
-    loc1 += 9;
-    bool littleEndian =
-            (header[loc1] == '<' || header[loc1] == '|' ? true : false);
-    assert(littleEndian);
-    (void)littleEndian;
-
-    // char type = header[loc1+1];
-    // assert(type == map_type(T));
-
-    std::string str_ws = header.substr(loc1 + 2);
-    loc2 = str_ws.find("'");
-    word_size = atoi(str_ws.substr(0, loc2).c_str());
-}
-
 NpyArray load_the_npy_file(FILE* fp) {
     // std::vector<size_t> shape;
     // size_t word_size;
