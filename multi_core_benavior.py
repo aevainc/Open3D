977662464737aa6f1c479ff4e9c19215a3be1f68
Yixing Lao <yixing.lao@gmail.com>
2021-10-04 09:38:24 -0700

    add one thread

--- multi_core_benavior.py
+++ multi_core_benavior.py
@@ -5,16 +5,30 @@ print(np)
 print(np.__version__)
 
 size = int(10e8)
-repeat = 10
+repeat = 3
 
-print("Numpy compute")
+print("Numpy compute binary")
+a = np.ones((size), dtype=np.float64)
+b = np.ones((size), dtype=np.float64)
+for i in range(repeat):
+    print(i)
+    c = a + b
+
+print("Numpy compute unary")
 a = np.ones((size), dtype=np.float64)
 for i in range(repeat):
     print(i)
-    a + a
+    b = np.sin(a)
 
-print("Open3D compute")
-a = o3c.Tensor.ones((int(10e8)))
+print("Open3D compute binary")
+a = o3c.Tensor.ones(size, dtype=o3c.float64)
+b = o3c.Tensor.ones(size, dtype=o3c.float64)
 for i in range(repeat):
     print(i)
-    a + a
+    c = a + b
+
+print("Open3D compute unary")
+a = o3c.Tensor.ones(size, dtype=o3c.float64)
+for i in range(repeat):
+    print(i)
+    b = a.sin()
