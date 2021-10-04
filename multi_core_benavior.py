192f6fa02369a6e6431df1c2975e0adc2745dba1
Yixing Lao <yixing.lao@gmail.com>
2021-10-04 03:45:10 -0700

    double add

--- multi_core_benavior.py
+++ multi_core_benavior.py
@@ -1,17 +1,20 @@
 import numpy as np
 import open3d.core as o3c
 
+print(np)
+print(np.__version__)
+
 size = int(10e8)
 repeat = 10
 
 print("Numpy compute")
-a = np.ones((size))
+a = np.ones((size), dtype=np.float64)
 for i in range(repeat):
     print(i)
-    a * a
+    a + a
 
 print("Open3D compute")
 a = o3c.Tensor.ones((int(10e8)))
 for i in range(repeat):
     print(i)
-    a * a
+    a + a
