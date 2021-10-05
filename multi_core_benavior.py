41b4c006f8bd1a5066efede88875c435a3b45242
Yixing Lao <yixing.lao@gmail.com>
2021-10-04 22:56:54 -0700

    add regular plot

--- multi_core_benavior.py
+++ multi_core_benavior.py
@@ -7,28 +7,38 @@ print(np.__version__)
 size = int(10e8)
 repeat = 3
 
-print("Numpy compute binary")
-a = np.ones((size), dtype=np.float64)
-b = np.ones((size), dtype=np.float64)
-for i in range(repeat):
-    print(i)
-    c = a + b
+a = np.ones((2, 3), dtype=np.int32)
+b = np.ones((2, 3), dtype=np.int32)
+c = a + b
 
-print("Numpy compute unary")
-a = np.ones((size), dtype=np.float64)
-for i in range(repeat):
-    print(i)
-    b = np.sin(a)
+a = np.ones((200, 300), dtype=np.int32)
+b = np.ones((200, 300), dtype=np.int32)
+c = a + b
 
-print("Open3D compute binary")
-a = o3c.Tensor.ones(size, dtype=o3c.float64)
-b = o3c.Tensor.ones(size, dtype=o3c.float64)
-for i in range(repeat):
-    print(i)
-    c = a + b
+###########################################
 
-print("Open3D compute unary")
-a = o3c.Tensor.ones(size, dtype=o3c.float64)
-for i in range(repeat):
-    print(i)
-    b = a.sin()
+# print("Numpy compute binary")
+# a = np.ones((size), dtype=np.int32)
+# b = np.ones((size), dtype=np.int32)
+# for i in range(repeat):
+#     print(i)
+#     c = a + b
+
+# print("Numpy compute unary")
+# a = np.ones((size), dtype=np.int32)
+# for i in range(repeat):
+#     print(i)
+#     b = np.sin(a)
+
+# print("Open3D compute binary")
+# a = o3c.Tensor.ones(size, dtype=o3c.int32)
+# b = o3c.Tensor.ones(size, dtype=o3c.int32)
+# for i in range(repeat):
+#     print(i)
+#     c = a + b
+
+# print("Open3D compute unary")
+# a = o3c.Tensor.ones(size, dtype=o3c.int32)
+# for i in range(repeat):
+#     print(i)
+#     b = a.sin()
