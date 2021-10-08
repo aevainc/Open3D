0f97da0cf180084e99377cb2996c5cb5c79039d1
Yixing Lao <yixing.lao@gmail.com>
2021-10-08 04:13:00 -0700

    simplier benchmark

--- benchmark.py
+++ benchmark.py
@@ -4,11 +4,13 @@ import open3d.core as o3c
 import time
 import os
 
-print(np)
-print(np.__version__)
+print("##############################")
+
+# print(np)
+# print(np.__version__)
 
 size = int(10e8)
-repeat = 10
+repeat = 5
 
 # a = np.ones((2, 3), dtype=np.int32)
 # b = np.ones((2, 3), dtype=np.int32)
@@ -21,9 +23,6 @@ repeat = 10
 ###########################################
 
 omp = os.getenv("OMP_NUM_THREADS")
-print(omp)
-
-print("##############################")
 
 # print("Numpy compute binary op")
 a = np.ones((size), dtype=np.int32)
@@ -48,5 +47,35 @@ start_time = time.time()
 for i in range(repeat):
     c = a + b
 print(f"OMP={omp}, contiguous, Open3D: {time.time() - start_time} sec")
+
+# print("Numpy compute binary op")
+a_ = np.ones((size), dtype=np.int32)
+b_ = np.ones((size), dtype=np.int32)
+a = a_[0:-1:2]
+b = b_[0:-1:2]
+start_time = time.time()
+for i in range(repeat):
+    c = a + b
+print(f"OMP={omp}, non-contiguous, Numpy: {time.time() - start_time} sec")
+
+# print("Torch compute binary op")
+a_ = torch.ones(size, dtype=torch.int32)
+b_ = torch.ones(size, dtype=torch.int32)
+a = a_[0:-1:2]
+b = b_[0:-1:2]
+start_time = time.time()
+for i in range(repeat):
+    c = a + b
+print(f"OMP={omp}, non-contiguous, Torch: {time.time() - start_time} sec")
+
+# print("Open3D compute binary op")
+a_ = o3c.Tensor.ones(size, dtype=o3c.int32)
+b_ = o3c.Tensor.ones(size, dtype=o3c.int32)
+a = a_[0:-1:2]
+b = b_[0:-1:2]
+start_time = time.time()
+for i in range(repeat):
+    c = a + b
+print(f"OMP={omp}, non-contiguous, Open3D: {time.time() - start_time} sec")
 
 print("##############################")
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,4 +3,3 @@
 python benchmark.py
 OMP_NUM_THREADS=1 python benchmark.py
 OMP_NUM_THREADS=2 python benchmark.py
-OMP_NUM_THREADS=18 python benchmark.py
