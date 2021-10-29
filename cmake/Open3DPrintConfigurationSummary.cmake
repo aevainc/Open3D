774cfd5eb779e9df203b01640455b67b7a6a6710
Yixing Lao <yixing.lao@gmail.com>
2021-10-29 02:45:48 -0700

    remove print

--- cmake/Open3DPrintConfigurationSummary.cmake
+++
@@ -1,7 +0,0 @@
-# Internal helper function.
-function(open3d_aligned_print printed_name printed_valued)
-    string(LENGTH "${printed_name}" PRINTED_NAME_LENGTH)
-    math(EXPR PRINTED_DOTS_LENGTH "40 - ${PRINTED_NAME_LENGTH}")
-    string(REPEAT "." ${PRINTED_DOTS_LENGTH} PRINTED_DOTS)
-    message(STATUS "  ${printed_name} ${PRINTED_DOTS} ${printed_valued}")
-endfunction()
