1a3b1fe5333c3841e4b8f927be1d3dbcb0b422c6
Yixing Lao <yixing.lao@gmail.com>
2021-10-29 03:22:53 -0700

    only keep main.cpp

--- Main.cpp
+++ Main.cpp
@@ -24,6 +24,36 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#include "Open3D.h"
+#include <CL/sycl.hpp>
+#include <array>
+#include <chrono>
+#include <iostream>
+
+namespace open3d {
+
+void RunSYCLDemo() {
+    using namespace sycl;
+
+    std::cout << "Hello RunSYCLDemo()" << std::endl;
+
+    constexpr int size = 16;
+    std::array<int, size> data;
+    // Create queue on implementation-chosen default device
+    sycl::queue Q;
+    // Create buffer using host allocated "data" array
+    buffer B{data};
+    Q.submit([&](handler &h) {
+        accessor A{B, h};
+        h.parallel_for(size, [=](auto &idx) { A[idx] = idx; });
+    });
+    // Obtain access to buffer on the host
+    // Will wait for device kernel to execute to generate data
+    host_accessor A{B};
+    for (int i = 0; i < size; i++) {
+        std::cout << "data[" << i << "] = " << A[i] << "\n";
+    }
+}
+
+}  // namespace open3d
 
 int main() { open3d::RunSYCLDemo(); }
