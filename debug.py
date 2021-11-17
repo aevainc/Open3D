843aebaf76417fcd627f0a360d73aef70d91d10f
Yixing Lao <yixing.lao@gmail.com>
2021-11-16 20:18:38 -0800

    add current branch test

--- debug.py
+++ debug.py
@@ -70,17 +70,17 @@ if __name__ == "__main__":
     start = time.time()
     with o3d.utility.VerbosityContextManager(
             o3d.utility.VerbosityLevel.Debug) as cm:
-        mesh_optimized = o3d.pipelines.color_map.run_rigid_optimizer(
+        mesh_optimized_rigid = o3d.pipelines.color_map.run_rigid_optimizer(
             mesh, rgbd_images, camera_trajectory,
             o3d.pipelines.color_map.RigidOptimizerOption(maximum_iteration=1))
     print(f"Rigid optimization takes {time.time() - start}")
 
-    # # Non-rigid Optimization
-    # start = time.time()
-    # with o3d.utility.VerbosityContextManager(
-    #         o3d.utility.VerbosityLevel.Debug) as cm:
-    #     mesh_optimized = o3d.pipelines.color_map.run_non_rigid_optimizer(
-    #         mesh, rgbd_images, camera_trajectory,
-    #         o3d.pipelines.color_map.NonRigidOptimizerOption(
-    #             maximum_iteration=100))
-    # print(f"Non-rigid optimization takes {time.time() - start}")
+    # Non-rigid Optimization
+    start = time.time()
+    with o3d.utility.VerbosityContextManager(
+            o3d.utility.VerbosityLevel.Debug) as cm:
+        mesh_optimized_nonrigid = o3d.pipelines.color_map.run_non_rigid_optimizer(
+            mesh_optimized_rigid, rgbd_images, camera_trajectory,
+            o3d.pipelines.color_map.NonRigidOptimizerOption(
+                maximum_iteration=100))
+    print(f"Non-rigid optimization takes {time.time() - start}")
